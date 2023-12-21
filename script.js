25870b017e8cfcc5bdbb3324bf91f189a5849ea0
Kent9-ops <154231776+Kent9-ops@users.noreply.github.com>
2023-12-21 17:34:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ document.body.appendChild(container); // Append container to the body
 
 // Create the paragraph element (text)
 let p = document.createElement("p");
-p.innerText = "Ai luat una dintre cele mai bune decizii din viața ta.  🎉 \nTrimite-mi un mesaj pe telegram! \nPutem iesi doar la o plimbarica daca doresti sau sa bem doar ceva 😉 \nSper ca iti place fetita dansatoare, sper ca o recunosti";
+p.innerText = "Ai luat una dintre cele mai bune decizii din viața ta.   \nTrimite-mi un mesaj pe telegram! \nPutem iesi doar la o plimbarica daca doresti sau sa bem doar ceva  \nSper ca iti place fetita dansatoare";
 p.style.fontSize = "2rem";
 p.style.fontWeight = "bold";
 container.appendChild(p); // Append text to the container
